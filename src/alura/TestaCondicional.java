1e5309c4da351b9aa4acd56eafe332fa64bea2d2
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-12 09:26:37 -0300

    Add conditional with one-line if

---
+++ src/alura/TestaCondicional.java
@@ -0,0 +1,10 @@
+package alura;
+
+public class TestaCondicional {
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais");
+		int idade = 20;
+		if (idade >= 18)
+			System.out.println("Você tem idade igual ou superior à 18 anos!");
+	}
+}
