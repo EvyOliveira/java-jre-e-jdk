2a315a7375f1dacc7982f5e5fe9a28198b216aa8
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-12 09:30:54 -0300

    Refactor if else conditional and line break

--- src/alura/TestaCondicional.java
+++ src/alura/TestaCondicional.java
@@ -3,8 +3,17 @@ package alura;
 public class TestaCondicional {
 	public static void main(String[] args) {
 		System.out.println("Testando condicionais");
-		int idade = 20;
-		if (idade >= 18)
-			System.out.println("Você tem idade igual ou superior à 18 anos!");
+		
+		int idade = 16;
+		int quantidadePessoas = 3;
+		if (idade >= 18) {
+			System.out.println("Seja bem-vindo.");
+		} else {
+			if(quantidadePessoas >= 2) {
+				System.out.println("Você não tem 18 anos, mas pode entrar, "
+						+ "pois está acompanhado!");
+			}
+			System.out.println("Infelizmente, você não pode entrar.");
+		}
 	}
 }
