4ae2f398901a578c02a98cd9ab6980158206d10e
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-12 09:36:19 -0300

    Add scope of variables of type int and boolean

---
+++ src/alura/TestaEscopo.java
@@ -0,0 +1,24 @@
+package alura;
+
+public class TestaEscopo {
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais");
+
+		int idade = 16;
+		int quantidadePessoas = 3;
+		boolean acompanhado;
+
+		if(quantidadePessoas >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+
+		System.out.println("Valor de acompanhado = " + acompanhado);
+		if (idade >= 18 || acompanhado == true) {
+			System.out.println("Seja bem-vindo.");
+		} else {
+			System.out.println("Infelizmente, você não pode entrar.");
+		}
+	}
+}
